2c9a7d8d88315e33ef6dea5fb7aa4b54a6c65489
PSongs <ponsongvilay@outlook.com>
2024-01-23 15:13:39 -0600

    Client - Orders features

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,12 +11,14 @@ import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
 import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
+import { OrderDetailedComponent } from './order-detailed/order-detailed.component';
 // import {BreadcrumbModule} from 'xng-breadcrumb';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    OrderDetailedComponent,
     
   ],
   imports: [
